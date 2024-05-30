00c1fd08215032dbcb9e7641bfcb658ba513caf1
MosesChisz <mosesc@cortexware.co.za>
2024-05-30 14:55:14 +0200

    three

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,22 +1,20 @@
 import React, { ChangeEvent, useState, SyntheticEvent } from 'react'
 
-type Props = {}
+interface Props {
+    onSearchSubmit: (e: SyntheticEvent) => void;
+    search: string | undefined;
+    handleSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
 
-const Search: React.FC<Props> = (props: Props) : JSX.Element => {
-    const [search, setSearch] = useState<string>("");
+const Search: React.FC<Props> = ({onSearchSubmit, search, handleSearchChange}: Props) : JSX.Element => {
+   
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
-    }
-
-    const onClick = (e: SyntheticEvent) => {
-        console.log(e);
-    }
   return (
-    <div>
-        <input value={search} onChange={(e) => handleChange(e)} />
-        <button onClick={(e) => onClick(e)} />
-    </div>
+    <>
+      <form onSubmit={onSearchSubmit}>
+          <input value={search} onChange={handleSearchChange} />
+      </form>
+    </>
   )
 }
 
